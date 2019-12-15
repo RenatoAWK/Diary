0b01ffa09592d13c5cee222e81feaec2e66c07b5
Renato <Renatoawk@gmail.com>
2019-12-15 12:18:16 -0300

    Chamada da tela de nota

--- app/src/main/java/com/renatoawk/diary/gui/NotesActivity.java
+++ app/src/main/java/com/renatoawk/diary/gui/NotesActivity.java
@@ -1,21 +1,24 @@
 package com.renatoawk.diary.gui;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.Window;
+import android.view.View;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.material.bottomappbar.BottomAppBar;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.renatoawk.diary.BottomNavigationDrawerFragment;
 import com.renatoawk.diary.R;
 
 public class NotesActivity extends AppCompatActivity {
+    private BottomAppBar bottomAppBar;
+    private FloatingActionButton fab;
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -29,10 +32,12 @@ public class NotesActivity extends AppCompatActivity {
             BottomNavigationDrawerFragment bottomNavigationDrawerFragment = new BottomNavigationDrawerFragment(this);
             bottomNavigationDrawerFragment.show(getSupportFragmentManager(), bottomNavigationDrawerFragment.getTag());
 
-
-
         } else if (item.getItemId() == R.id.app_bar_settings){
             Toast.makeText(this, "Pegou o clique no settings", Toast.LENGTH_SHORT).show();
+
+        }  else if (item.getItemId() == R.id.app_bar_search){
+            Toast.makeText(this, "Pegou o clique no search", Toast.LENGTH_SHORT).show();
+
         }
         return true;
     }
@@ -42,8 +47,17 @@ public class NotesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notes);
 
-        BottomAppBar bottomAppBar = findViewById(R.id.app_bar);
+        bottomAppBar = findViewById(R.id.app_bar);
+        fab = findViewById(R.id.fab);
         setSupportActionBar(bottomAppBar);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent noteActivity = new Intent(getApplicationContext(), NoteActivity.class);
+                startActivity(noteActivity);
+            }
+        });
 
 
 
